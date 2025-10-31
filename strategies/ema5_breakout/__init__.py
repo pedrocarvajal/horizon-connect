8eca29541b82e266afc987afa548071df3d87a8f
pedrocarvajal <pedro@memeonlyme.com>
2025-10-31 11:00:01 +0400

    Update backtest date range to 1 year, refactor MAIndicator and related classes for improved functionality
    - Changed the backtest date range from 6 years to 1 year for more focused testing.
    - Deleted the old MAIndicator implementation and replaced it with a new version that includes exponential moving averages and better logging.
    - Introduced MAValueModel for handling moving average values.
    - Refactored CandleInterface to provide direct access to candles and updated IndicatorInterface to use a values list instead of elements.
    - Enhanced EMA5BreakoutStrategy to integrate the new MAIndicator for improved trading strategy performance.

--- strategies/ema5_breakout/__init__.py
+++ strategies/ema5_breakout/__init__.py
@@ -1,4 +1,5 @@
 from enums.timeframe import Timeframe
+from indicators.ma import MAIndicator
 from models.candlestick import CandlestickModel
 from models.tick import TickModel
 from services.candle import CandleService
@@ -25,37 +26,46 @@ class EMA5BreakoutStrategy(StrategyService):
             ),
         }
 
+        self._indicators = {
+            "ema5": MAIndicator(
+                period=5,
+                exponential=True,
+                candles=self._candles[Timeframe.ONE_HOUR].candles,
+            ),
+        }
+
     def on_tick(self, tick: TickModel) -> None:
         super().on_tick(tick)
 
+    def _is_ready(self) -> bool:
+        return (
+            len(self._candles[Timeframe.ONE_HOUR].get(0)) > 0
+            and len(self._candles[Timeframe.ONE_DAY].get(0)) > 0
+        )
+
     def _on_close_1h_candle(self, candle: CandlestickModel) -> None:
-        # open = candle.open_price
-        # high = candle.high_price
-        # low = candle.low_price
-        # close = candle.close_price
-        # open_time = candle.kline_open_time
-        # close_time = candle.kline_close_time
+        self._indicators["ema5"].refresh()
+        open_time = candle.kline_open_time
+        open_price = candle.open_price
+        close_time = candle.kline_close_time
+        close_price = candle.close_price
+        high_price = candle.high_price
+        low_price = candle.low_price
 
-        # self._log.info(
-        #     f"[1H Candle Closed] "
-        #     f"Time: {open_time:%Y-%m-%d %H:%M} → {close_time:%Y-%m-%d %H:%M} | "
-        #     f"O: {open:.2f} H: {high:.2f} L: {low:.2f} C: {close:.2f}"
-        # )
+        if len(self._indicators["ema5"].values) > 0:
+            ema5 = self._indicators["ema5"].values[-1]
+            ema5_date = ema5.date
+            ema5_value = ema5.value
 
-        pass
+            self._log.info(
+                f"Open time: {open_time} | "
+                f"Open price: {open_price} | "
+                f"Close time: {close_time} | "
+                f"Close price: {close_price} | "
+                f"High price: {high_price} | "
+                f"Low price: {low_price} | "
+                f"EMA5: ({ema5_date}) {ema5_value}"
+            )
 
     def _on_close_1d_candle(self, candle: CandlestickModel) -> None:
-        # open = candle.open_price
-        # high = candle.high_price
-        # low = candle.low_price
-        # close = candle.close_price
-        # open_time = candle.kline_open_time
-        # close_time = candle.kline_close_time
-
-        # self._log.info(
-        #     f"[1D Candle Closed] "
-        #     f"Time: {open_time:%Y-%m-%d %H:%M} → {close_time:%Y-%m-%d %H:%M} | "
-        #     f"O: {open:.2f} H: {high:.2f} L: {low:.2f} C: {close:.2f}"
-        # )
-
         pass
