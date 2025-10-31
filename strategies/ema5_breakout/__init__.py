7e51f93891be44068db6efa2d30acbd2b34ba4aa
pedrocarvajal <pedro@memeonlyme.com>
2025-10-31 11:16:22 +0400

    Refactor MAIndicator and EMA5BreakoutStrategy for improved clarity and functionality
    - Introduced a multiplier coefficient in MAIndicator for better control over exponential moving average calculations.
    - Simplified the on_tick method in MAIndicator to enhance readability and maintainability.
    - Updated EMA5BreakoutStrategy to streamline candle handling and improve logging of EMA5 values.

--- strategies/ema5_breakout/__init__.py
+++ strategies/ema5_breakout/__init__.py
@@ -20,10 +20,7 @@ class EMA5BreakoutStrategy(StrategyService):
         self._candles = {
             Timeframe.ONE_HOUR: CandleService(
                 timeframe=Timeframe.ONE_HOUR, on_close=self._on_close_1h_candle
-            ),
-            Timeframe.ONE_DAY: CandleService(
-                timeframe=Timeframe.ONE_DAY, on_close=self._on_close_1d_candle
-            ),
+            )
         }
 
         self._indicators = {
@@ -38,26 +35,11 @@ class EMA5BreakoutStrategy(StrategyService):
     def on_tick(self, tick: TickModel) -> None:
         super().on_tick(tick)
 
-    def _is_ready(self) -> bool:
-        return (
-            len(self._candles[Timeframe.ONE_HOUR].get(0)) > 0
-            and len(self._candles[Timeframe.ONE_DAY].get(0)) > 0
-        )
-
     def _on_close_1h_candle(self, candle: CandlestickModel) -> None:
-        open_time = candle.kline_open_time
-        open_price = candle.open_price
-        close_time = candle.kline_close_time
-        close_price = candle.close_price
-        high_price = candle.high_price
-        low_price = candle.low_price
-
-        if len(self._indicators["ema5"].values) > 0:
-            ema5 = self._indicators["ema5"].values[-1]
-            ema5_date = ema5.date
-            ema5_value = ema5.value
+        ema5 = self._indicators["ema5"]
+        if len(ema5.values) > 0:
+            ema5_value = ema5.values[-1]
+            ema5_date = ema5_value.date
+            ema5_value = ema5_value.value
 
             self._log.info(f"EMA5: ({ema5_date}) {ema5_value}")
-
-    def _on_close_1d_candle(self, candle: CandlestickModel) -> None:
-        pass
